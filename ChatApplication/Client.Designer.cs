41ea073de42e11acae9e6946be16350671cbd603
2540136850@qq.com <2540136850@qq.com>
2020-02-07 17:09:55 +0800

    socket communicator first commit

---
+++ ChatApplication/Client.Designer.cs
@@ -0,0 +1,120 @@
+﻿namespace ChatApplication
+{
+    partial class Client
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textIP = new System.Windows.Forms.TextBox();
+            this.textPort = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textLog = new System.Windows.Forms.TextBox();
+            this.textSend = new System.Windows.Forms.TextBox();
+            this.btnSend = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // textIP
+            // 
+            this.textIP.Location = new System.Drawing.Point(67, 25);
+            this.textIP.Name = "textIP";
+            this.textIP.Size = new System.Drawing.Size(164, 25);
+            this.textIP.TabIndex = 0;
+            this.textIP.Text = "192.168.1.101";
+            // 
+            // textPort
+            // 
+            this.textPort.Location = new System.Drawing.Point(248, 25);
+            this.textPort.Name = "textPort";
+            this.textPort.Size = new System.Drawing.Size(100, 25);
+            this.textPort.TabIndex = 1;
+            this.textPort.Text = "50000";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(363, 25);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(79, 25);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "连接";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textLog
+            // 
+            this.textLog.Location = new System.Drawing.Point(67, 56);
+            this.textLog.Multiline = true;
+            this.textLog.Name = "textLog";
+            this.textLog.Size = new System.Drawing.Size(520, 139);
+            this.textLog.TabIndex = 3;
+            // 
+            // textSend
+            // 
+            this.textSend.Location = new System.Drawing.Point(67, 201);
+            this.textSend.Multiline = true;
+            this.textSend.Name = "textSend";
+            this.textSend.Size = new System.Drawing.Size(520, 107);
+            this.textSend.TabIndex = 4;
+            // 
+            // btnSend
+            // 
+            this.btnSend.Location = new System.Drawing.Point(67, 324);
+            this.btnSend.Name = "btnSend";
+            this.btnSend.Size = new System.Drawing.Size(96, 30);
+            this.btnSend.TabIndex = 5;
+            this.btnSend.Text = "发送消息";
+            this.btnSend.UseVisualStyleBackColor = true;
+            this.btnSend.Click += new System.EventHandler(this.btnSend_Click);
+            // 
+            // Client
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnSend);
+            this.Controls.Add(this.textSend);
+            this.Controls.Add(this.textLog);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textPort);
+            this.Controls.Add(this.textIP);
+            this.Name = "Client";
+            this.Text = "客户端聊天器";
+            this.Load += new System.EventHandler(this.Client_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textIP;
+        private System.Windows.Forms.TextBox textPort;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textLog;
+        private System.Windows.Forms.TextBox textSend;
+        private System.Windows.Forms.Button btnSend;
+    }
+}
+
---
+++ ChatApplication/Client.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ChatApplication
+{
+    public partial class Client : Form
+    {
+        public Client()
+        {
+            InitializeComponent();
+        }
+
+        Socket socketSend;
+        //客户端连接服务器端
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //创建一个socket用于通信
+            socketSend = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            IPAddress ip = IPAddress.Parse(textIP.Text);
+            string port = textPort.Text;
+            IPEndPoint point = new IPEndPoint(ip, Convert.ToInt32(port));
+            socketSend.Connect(point);
+            ShowMsg("连接成功！");
+
+            Thread th = new Thread(Recieve);
+            th.IsBackground = true;
+            th.Start();
+
+        }
+
+        //接收服务器端消息
+        void Recieve()
+        {
+            while (true)
+            {
+                try
+                {
+
+                    byte[] buffer = new byte[2 * 1024 * 1024];
+                    int r = socketSend.Receive(buffer);
+                    if (r == 0)
+                    {
+                        break;
+                    }
+                    if (buffer[0] == 0)
+                    {
+                        ShowMsg(socketSend.RemoteEndPoint.ToString() + ":" + Encoding.UTF8.GetString(buffer,1,r-1));
+                    }
+                    else if (buffer[0] == 1)
+                    {
+                        SaveFileDialog sfd = new SaveFileDialog();
+                        sfd.InitialDirectory = @"E:\Desktop";
+                        sfd.Title = "请保存文件";
+                        sfd.Filter = "所有文件|*.*";
+                        sfd.ShowDialog(this);
+                        string filePath = sfd.FileName;
+                        using (FileStream fs = new FileStream(filePath, FileMode.OpenOrCreate, FileAccess.Write))
+                        {
+                            fs.Write(buffer, 1, r - 1);
+                            ShowMsg("保存文件成功！");
+                        }
+                    }
+                    else if (buffer[0] == 2)
+                    {
+                        ZD();
+                    }
+                }
+                catch
+                {
+
+                }
+            }
+        }
+
+        void ZD()
+        {
+            for (int i = 0; i < 500; i++)
+            {
+                this.Location = new Point(this.Location.X + 1, this.Location.Y + 1);
+                this.Location = new Point(this.Location.X - 1, this.Location.Y - 1);
+            }
+        }
+
+        void ShowMsg(string str)
+        {
+            textLog.AppendText(str + "\r\n");
+        }
+
+        private void Client_Load(object sender, EventArgs e)
+        {
+            Control.CheckForIllegalCrossThreadCalls = false;
+        }
+
+        //发送消息
+        private void btnSend_Click(object sender, EventArgs e)
+        {
+            byte[] buffer = System.Text.Encoding.UTF8.GetBytes(textSend.Text.Trim());
+            socketSend.Send(buffer);
+        }
+    }
+}
